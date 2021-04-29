82f67e5b2fc413055b27a627dffaf60612ff527b
whoisbot <dlppycz@163.com>
2021-04-29 14:45:49 +0800

    q

--- youthzl.js
+++ youthzl.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 
- 
+ //随机si
     function randomString() {
       let len = 32;
       let $chars = "abcdefhijkmnprstwxyz123456789";
@@ -12,19 +12,20 @@ const axios = require('axios');
       }
       return pwd;
     }
+    
+    
+    
+    //获取分享时间字符串
     var q=Date.parse(new Date())/1000;
     
-
+//get 函数
     var aa=function(){
       
         var j = randomString();
  var p = new Date().getTime();
     const url = 'https://script.baertt.com/count2/callback?si='+j+'&referer=https%253A%252F%252Ffocus.youth.cn%252Farticle%252Fs%253Fsignature%253DQB5EzPY3exK9wOd7E9kvgruV6M9PFgWkqzQ78oADjvkbgZRGLV%2526uid%253D51526616%2526phone_code%253Da2823679662e562c3bb1fade2b2f3d5b%2526scid%253D37478516%2526time%253D'+q+'%2526app_version%253D2.0.2%2526sign%253Db2787ec49cfd5780220a22c041d1c051&_='+p+'&jsonpcallback=jsonp5';
     
-
-    
-    
-     return axios.get(url, {
+    return axios.get(url, {
     
       headers: {'Referer' : 'https://focus.youth.cn/',
     'Host' : 'script.baertt.com',
@@ -42,14 +43,21 @@ const axios = require('axios');
   }
 
 
+
+
 const sleep = function (ms){
   return new Promise(resolve => setTimeout(resolve, ms))
+  
 }
 const request = async function(){
   for(let i=0;i<50;i++){
     aa();
     var num=Math.ceil(Math.random() * 25);
-    await sleep(num*200);
+    await sleep(num*500);
   }
 }
+
+
+
+
 request();
