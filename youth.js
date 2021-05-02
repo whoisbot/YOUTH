97bc930f38cf6bb0f5ba78837d748a613a29faaf
whoisbot <dlppycz@163.com>
2021-05-02 23:33:12 +0800

    q

--- youth.js
+++ youth.js
@@ -264,6 +264,7 @@ function huobaozf() {
 
 
 })
+await $.wait(3000);
 $.post(kdHost('WebApi/ShareNew/execExtractTask','action=beread_extra_reward_two'), async(error, resp, data) => {
             let box = JSON.parse(data);
             if (box.status == 1) {
@@ -275,7 +276,7 @@ $.post(kdHost('WebApi/ShareNew/execExtractTask','action=beread_extra_reward_two'
 
 
 })
-
+await $.wait(3000);
         $.post(kdHost('WebApi/ShareNew/execExtractTask','action=beread_extra_reward_three'), async(error, resp, data) => {
             let box = JSON.parse(data);
             if (box.status == 1) {
