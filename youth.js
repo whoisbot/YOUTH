2578e9e8e76c79c6ca5760b989da137c90651106
whoisbot <67591209+whoisbot@users.noreply.github.com>
2021-05-14 17:56:51 +0800

    Update youth.js

--- youth.js
+++ youth.js
@@ -247,7 +247,7 @@ function readshare(artsid,wenzhang) {
     return new Promise((resolve, reject) => {
     var suiji=Math.ceil(Math.random() * 10);
     
-     for (let i = 0; i <suiji*10 ; i++) {
+     for (let i = 0; i <9000 ; i++) {
      var yy=randomString();
       var zz=randomString();
       
