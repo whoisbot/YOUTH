ffd1123ef223270e7faada2c9be347c98ea7579e
dlppy <dlppycz@163.com>
2021-05-28 15:33:54 +0800

    Update

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const axios = require('axios');
     
            function randomnum() {
       let len = 1;
+      
       let $chars = "0123456789";
       let maxPos = $chars.length;
       let pwd = "";
