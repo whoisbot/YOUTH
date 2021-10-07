ffe510a2bea365dd8e797385f174107c23c03f73
whoisbot <67591209+whoisbot@users.noreply.github.com>
2021-10-07 21:56:46 +0800

    Update wlxin.js

--- wlxin.js
+++ wlxin.js
@@ -88,6 +88,7 @@ const hero=data.token+" "+data.user_name;
 array.push(hero);
 
 $store.put(array,'token');
+Detail(kdcookie,kdcookie2,pinpai,shebei);
 
   }).catch((err) => {
       //catch 失败
@@ -100,8 +101,91 @@ $store.put(array,'token');
   
   }
 
+function cash(kdcookie,pinpai,shebei){
 
+    axios({
+    url : 'https://wlkdapi.zhongchuanjukan.com/withdraw/wx',
+method: 'post',
+        data:
+{
+  "userid": null,
+  "device_userid": "",
+  "sysname": "wlkd",
+  "appversioncode": null,
+  "appversion": null,
+  "brand": pinpai,
+  "model": shebei,
+  "token": kdcookie,
+  "channel": null,
+  "optime": 1633144311,
+  "skey": null,
+  "adv_provider": null,
+  "os": "android",
+  "osversion": null,
+  "darkmode": 1,
+  "threadid": null,
+  "spread": null,
+  "from": null,
+  "remarks": null,
+  "ip": null,
+  "province": null,
+  "city": null,
+  "hour": null,
+  "date": null,
+  "time": null,
+  "requestid": null,
+  "event": null,
+  "profit": 300000
+},
+   headers:
+{"Accept": "*/*","Accept-Encoding": "gzip, deflate, br","Accept-Language": "zh-cn","Connection": "keep-alive","Content-Type": "application/json;charset=utf-8","Host": "wlkdapi.zhongchuanjukan.com","Origin": "https://wlkdapi.zhongchuanjukan.com","User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 14_7_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148","X-Requested-With": "XMLHttpRequest"}
+    }).then((res) => { 
+      //then 成功
+ const data = res.data;
 
+console.log(data.msg_desc);
+  }).catch((err) => {
+      //catch 失败
+      console.log(err)
+
+    }); 
+  }
+
+function Detail(kdcookie,kdcookie2,pinpai,shebei){
+
+    axios({
+    url : 'https://wlkdapi.zhongchuanjukan.com/account/getTodayDetail',
+method: 'post',
+        data:
+{
+  "channel": "AppStore",
+  "userid": "0427bea519444323a36f63ee2fc6ade1",
+  "appversioncode": "201",
+  "brand": pinpai,
+  "sysname": "wlkd",
+  "appversion": "2.0.1",
+  "os": "ios",
+  "token": kdcookie,
+  "model": shebei,
+  "osversion": "14.7.1",
+  "device_userid": ""
+},
+   headers:
+{"Accept": "*/*","Accept-Encoding": "gzip, deflate, br","Accept-Language": "zh-Hans-CN;q=1","Connection": "keep-alive","Content-Length": "337","Content-Type": "application/json","Host": "wlkdapi.zhongchuanjukan.com","User-Agent": "wen lu kan dian/2.0.1 (iPhone; iOS 14.7.1; Scale/2.00)","sppid":kdcookie2}
+    }).then((res) => { 
+      //then 成功
+ const data = res.data;
+const money=data.balance_m;
+ console.log(money);
+if(money>30){
+cash(kdcookie,pinpai,shebei);
+}
+  }).catch((err) => {
+      //catch 失败
+      console.log(err)
+
+    }); 
+  }
 
 const sleep = function (ms){
   return new Promise(resolve => setTimeout(resolve, ms))
